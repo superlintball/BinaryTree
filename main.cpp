14991d8e896d4e021724b94cf31470cfbf8db3a9
superlintball <ravnikator@gmail.com>
2018-03-15 11:26:06 -0700

    fixed that thing

--- main.cpp
+++ main.cpp
@@ -12,37 +12,37 @@ using namespace std;
 //creates a node object with a number, a left node, and a right node
 struct Node
 {
-	int num;
-	Node* left = NULL;
-	Node* right = NULL;
-	Node(int number)
-	{
+    int num;
+    Node* left = NULL;
+    Node* right = NULL;
+    Node(int number)
+    {
 		num = number;
-	}
+    }
 };
 
 //thanks to myself for writing the visualize code in shunting yard
 //print out the tree in a sideways form
 void visualize(Node* head, int depth=0)
 {
-	if(head->right)
+    if(head->right)
 		visualize(head->right, depth+1);
-	
-	for(int i = 0; i < depth; i++)
-	{
+
+    for(int i = 0; i < depth; i++)
+    {
 		cout << "\t";
-	}
-	
-	cout << head->num << endl;
-	
-	if(head->left)
+    }
+
+    cout << head->num << endl;
+
+    if(head->left)
 		visualize(head->left, depth+1);
 }
 
 //adds an integer to the binary tree recursively
 void add(Node* head, int toAdd)
 {
-	//if the integer being added is greater than the current node go right
+    //if the integer being added is greater than the current node go right
 	if(toAdd > head->num)
 	{
 		//if there is already a node to the right, go right
@@ -68,15 +68,15 @@ void add(Node* head, int toAdd)
 		{
 			head->left = new Node(toAdd);
 		}
-	}
+    }
 }
 
 //recursively finds a number in the tree, and returns the node above it
 Node* find(Node* head, int toRemove, Node* prev = NULL)
 {
-	//if the number being looked for is greater than the current, go right
-	if(toRemove > head->num)
-	{
+    //if the number being looked for is greater than the current, go right
+    if(toRemove > head->num)
+    {
 		if(head->right)
 		{
 			return find(head->right, toRemove, head);
@@ -86,15 +86,15 @@ Node* find(Node* head, int toRemove, Node* prev = NULL)
 		{
 			return NULL;
 		}
-	}
-	//when the number is found, return the node above it
-	else if(toRemove == head->num)
-	{
+    }
+    //when the number is found, return the node above it
+    else if(toRemove == head->num)
+    {
 		return prev;
-	}
-	//if the number being looked for is less than the current, go left
-	else if(toRemove < head->num)
-	{
+    }
+    //if the number being looked for is less than the current, go left
+    else if(toRemove < head->num)
+    {
 		if(head->left)
 		{
 			return find(head->left, toRemove, head);
@@ -104,16 +104,16 @@ Node* find(Node* head, int toRemove, Node* prev = NULL)
 		{
 			return NULL;
 		}
-	}
+    }
 }
 
 //removes a number from the tree
 void remove(Node* head, int toRemove)
 {
-	//the find function returns NULL if the number being removed is in the head, as there is no node above it
-	//thus, first check if the head contains the number
-	if(toRemove == head->num)
-	{
+    //the find function returns NULL if the number being removed is in the head, as there is no node above it
+    //thus, first check if the head contains the number
+    if(toRemove == head->num)
+    {
 		//if there is no left or right, the tree is empty
 		if(!head->left && !head->right)
 		{
@@ -149,29 +149,29 @@ void remove(Node* head, int toRemove)
 				upReplace = replace;
 				replace = replace->right;
 			}
-			
+
 			//remove the next smallest number from the tree
 			if(upReplace == head)
 				upReplace->left = replace->left;
 			else
 				upReplace->right = replace->left;
-			
+
 			//replace the head with its next smallest number
 			head->num = replace->num;
 			delete replace;
 		}
 		//exit the remove function
 		return;
-	}
-	
-	//now, the find function is run
-	Node* above = find(head, toRemove);
-	//if the number was found
-	if(above)
-	{
+    }
+
+    //now, the find function is run
+    Node* above = find(head, toRemove);
+    //if the number was found
+    if(above)
+    {
 		//store the node being removed and what side of "above" it is on
 		Node* temp = NULL;
-		
+
 		//left will be true and right will be false
 		bool side = true;
 		if(toRemove > above->num)
@@ -183,7 +183,7 @@ void remove(Node* head, int toRemove)
 		{
 			temp = above->left;
 		}
-		
+
 		//if there is no left or right, make the above node point to NULL where the node was
 		if(!temp->right && !temp->left)
 		{
@@ -191,7 +191,7 @@ void remove(Node* head, int toRemove)
 				above->left = NULL;
 			else
 				above->right = NULL;
-			
+
 			//delete the node
 			delete temp;
 		}
@@ -202,7 +202,7 @@ void remove(Node* head, int toRemove)
 				above->left = temp->left;
 			else
 				above->right = temp->left;
-			
+
 			//delete the node
 			delete temp;
 		}
@@ -213,7 +213,7 @@ void remove(Node* head, int toRemove)
 				above->left = temp->right;
 			else
 				above->right = temp->right;
-			
+
 			//delete the node
 			delete temp;
 		}
@@ -228,150 +228,147 @@ void remove(Node* head, int toRemove)
 				upReplace = replace;
 				replace = replace->right;
 			}
-			
+
 			//remove the next smallest number from its position in the tree
 			if(upReplace == temp)
 				upReplace->left = replace->left;
 			else
 				upReplace->right = replace->left;
-			
+
 			//replace the node with the next smallest number
 			temp->num = replace->num;
 			delete replace;
 		}
-	}
-	//if the number wasn't found, complain
-	else
-	{
+    }
+    //if the number wasn't found, complain
+    else
+    {
 		cout << "That number is not in the tree.\n";
-	}
+    }
 }
 
 int main()
 {
-	Node* tree = NULL;
-	bool fileFound = true;
-	
-	//thanks to myself for writing the input code in heap
-	//prompt the user for how numbers will be entered
-	cout << "Would you like to enter numbers via MANUAL input or from FILE? \n";
-	char* input = new char[8];
-	cin >> input;
-	
-	//if the user wants to enter numbers manually
-	if(!strcmp(input, "MANUAL") || !strcmp(input, "manual"))
-	{
-		//add numbers one by one until the user says they are done
-		cout << "Enter integers from 1-1000 one at a time. Enter DONE when you are done adding numbers \n";
+    Node* tree = NULL;
+    bool fileFound = true;
+    bool running = true;
+
+    while(running)
+    {
+		cout << "Would you like to INPUT numbers, REMOVE numbers, or QUIT?\n";
+		//thanks to myself for writing the input code in heap
+		char* input = new char[8];
 		cin >> input;
-		if(isdigit(input[0]))
-		{
-			tree = new Node(atoi(input));
-			cin >> input;
-		}
-		while(strcmp(input, "DONE") && strcmp(input, "done"))
-		{
-			int toAdd = atoi(input);
-			add(tree, toAdd);
-			cin >> input;
-		}
-	}
-	//if the user wants to enter numbers from a file
-	else if(!strcmp(input, "FILE") || !strcmp(input, "file"))
-	{
-		char* file = new char[32];
-		char* list = new char[512];
 		
-		//prompt the user for the name of the file
-		cout << "\nEnter the name of the file.\n";
-		cin.get();
-		cin.get(file, 32);
-		cin.get();
-		
-		//open the file
-		ifstream stream(file);
-		if(stream.is_open())
+		if(!strcmp(input, "INPUT") || !strcmp(input, "input"))
 		{
-			//extract the text from the file
-			stream.getline(list, 512);
-			stream.close();
-			cout << endl << list << endl;
-			//extract the numbers from the text
-			for(int i = 0; i < strlen(list); i++)
+			//prompt the user for how numbers will be entered
+			cout << "Would you like to enter numbers via MANUAL input or from FILE? \n";
+			cin >> input;
+			//if the user wants to enter numbers manually
+			if(!strcmp(input, "MANUAL") || !strcmp(input, "manual"))
 			{
-				if(isdigit(list[i]))
+				//add numbers one by one until the user says they are done
+				cout << "Enter integers from 1-1000 one at a time. Enter DONE when you are done adding numbers \n";
+				cin >> input;
+				if(isdigit(input[0]))
 				{
-					//isolate the number into a separate char pointer
-					int start = i;
-					char* number = new char[5];
-					do number[i-start] = list[i];
-					while(list[++i] != ' ' && i < strlen(list));
-					number[i-start] = '\0';
-					
-					//convert the isolated char pointer into an integer and add it to the tree
-					if(!tree)
+					tree = new Node(atoi(input));
+					cin >> input;
+				}
+				while(strcmp(input, "DONE") && strcmp(input, "done"))
+				{
+					int toAdd = atoi(input);
+					add(tree, toAdd);
+					cin >> input;
+				}
+			}
+			//if the user wants to enter numbers from a file
+			else if(!strcmp(input, "FILE") || !strcmp(input, "file"))
+			{
+				char* file = new char[32];
+				char* list = new char[512];
+
+				//prompt the user for the name of the file
+				cout << "\nEnter the name of the file.\n";
+				cin.get();
+				cin.get(file, 32);
+				cin.get();
+
+				//open the file
+				ifstream stream(file);
+				if(stream.is_open())
+				{
+					//extract the text from the file
+					stream.getline(list, 512);
+					stream.close();
+					cout << endl << list << endl;
+					//extract the numbers from the text
+					for(int i = 0; i < strlen(list); i++)
 					{
-						tree = new Node(atoi(number));
+						if(isdigit(list[i]))
+						{
+							//isolate the number into a separate char pointer
+							int start = i;
+							char* number = new char[5];
+							do number[i-start] = list[i];
+								while(list[++i] != ' ' && i < strlen(list));
+							number[i-start] = '\0';
+
+							//convert the isolated char pointer into an integer and add it to the tree
+							if(!tree)
+							{
+								tree = new Node(atoi(number));
+							}
+							else
+							{
+								int toAdd = atoi(number);
+								add(tree, toAdd);
+							}
+						}
 					}
+				}
+				else
+				{
+					cout << "File not found\n";
+					fileFound = false;
+				}
+			}
+			//if the user entered neither option, throw an error.
+			else
+			{
+				cout << "Sorry I couldn't understand that.\n";
+				fileFound = false;
+			}
+		}
+		else if(!strcmp(input, "REMOVE") || !strcmp(input, "remove"))
+		{
+			//if the tree exists
+			if(fileFound)
+			{
+				//print it out
+				visualize(tree);
+
+				//ask the user if they want to delete numbers and keep asking until they say no
+				cout << "Enter numbers to remove one at a time.\nEnter DONE when you are done removing numbers.\n";
+				//if the user wants to delete, delete
+				while(strcmp(input, "DONE") && strcmp(input, "done"))
+				{
+					cin >> input;
+					int toRemove = atoi(input);
+					cout << endl;
+
+					//remove the number from the tree
+					remove(tree, toRemove);
+
+					//if the tree is empty, end
+					if(!tree || tree->num == 0)
+						deleting = false;
 					else
-					{
-						int toAdd = atoi(number);
-						add(tree, toAdd);
-					}
+						visualize(tree);
 				}
 			}
 		}
-		else
-		{
-			cout << "File not found\n";
-			fileFound = false;
-		}
-	}
-	//if the user entered neither option, throw an error.
-	else
-	{
-		cout << "Sorry I couldn't understand that.\n";
-		fileFound = false;
-	}
-	
-	//if the tree exists
-	if(fileFound)
-	{
-		//print it out
-		visualize(tree);
-		
-		//ask the user if they want to delete numbers and keep asking until they say no
-		bool deleting = true;
-		while(deleting)
-		{
-			cout << "Would you like to remove a number from the tree? (y/n)\n";
-			cin >> input;
-			//if the user wants to delete, delete
-			if(!strcmp(input, "y") || !strcmp(input, "Y")
-				|| !strcmp(input, "Yes") || !strcmp(input, "yes") || !strcmp(input, "YES"))
-			{
-				//prompt the user for the number to be deleted
-				cout << "What number would you like to remove?\n";
-				int toRemove = -1;
-				cin >> toRemove;
-				cout << endl;
-				
-				//remove the number from the tree
-				remove(tree, toRemove);
-				
-				//if the tree is empty, end
-				if(!tree || tree->num == 0)
-					deleting = false;
-				else
-					visualize(tree);
-			}
-			//if the user is done deleting, end
-			else if(!strcmp(input, "n") || !strcmp(input, "N")
-				|| !strcmp(input, "No") || !strcmp(input, "no") || !strcmp(input, "NO"))
-			{
-				deleting = false;
-			}
-		}
-	}
+    }
     return 0;
 }
